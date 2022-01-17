68005339218f8b4afdf302c484cb6d1a54e31036
Dustin <dmwinslow@users.noreply.github.com>
2022-01-17 08:14:23 -0800

    Add additional rockprop and rockprop2 tests

--- matlab_archive/test/assert_files_match.m
+++ matlab_archive/test/assert_files_match.m
@@ -2,9 +2,8 @@ function assert_files_match(file1, file2)
     file1_contents = fileread(file1);
     file2_contents = fileread(file2);
     try
-        assert(strcmp(file1_contents, file2_contents));
+        assert(strcmp(file1_contents, file2_contents), '%s does not match %s', file1, file2);
     catch e
-        sprintf('Output (%s) does not match fixture (%s)', file1, file2)
         rethrow(e);
     end
 end
--- matlab_archive/test/test_rockprop2.m
+++ matlab_archive/test/test_rockprop2.m
@@ -1,35 +1,7 @@
-% test rockprop
+% test rockprop2
 
-%% Rockprop np2d_cond
-test_against_fixture('np2d_cond', 'cond');
-
-
-%% Rockprop np2d_p11
-test_against_fixture('np2d_p11', 'run');
-
-
-%% Rockprop np3d_cond
-test_against_fixture('np3d_cond', 'run');
-
-
-%% Rockprop np3d_p11
-test_against_fixture('np3d_p11', 'run');
-
-
-%% Rockprop jdf2d_p12
-test_against_fixture('jdf2d_p12', 'p12');
-
-
-%% Rockprop jdf3d_p12d_g981
-test_against_fixture('jdf3d_p12d_g981', 'p12d_g981');
-
-
-%% Rockprop jdf3d_p12
-test_against_fixture('jdf3d_p12', 'p12');
-
-
-%% Rockprop jdf3d_conduit_p12
-test_against_fixture('jdf3d_conduit_p12', 'p12');
+%% Rockprop jdf3d_deep_p11ani
+test_against_fixture('jdf3d_deep_p11ani', 'p11d600efto_GBv10BBv10_g981');
 
 
 function test_against_fixture(fixture_name, run_name)
@@ -52,7 +24,7 @@ function test_against_fixture(fixture_name, run_name)
             copyfile(input_path, '.');
         end
 
-        rockprop();
+        rockprop2();
 
         for k=1:length(output_extensions)
             filename = strcat(run_name, output_extensions{k});
