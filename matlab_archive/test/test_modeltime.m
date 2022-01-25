1aa57540776baeaf97ca3ae0531a85ee738a7f4d
Dustin <dmwinslow@users.noreply.github.com>
2022-01-25 02:21:51 -0800

    Fix test inputs for modeltime test

--- matlab_archive/test/test_modeltime.m
+++ matlab_archive/test/test_modeltime.m
@@ -1,11 +1,11 @@
 % test modeltime
 
 %% modeltime np2d_cond
-test_against_fixture('np2d_cond', 'cond');
+test_against_expected('np2d_cond', 36500000000, 18024418.611298);
 
 
 %% modeltime np3d_cond
-test_against_fixture('np3d_cond', 'run');
+test_against_expected('np3d_cond', 5475000000, 1338452148.437500);
 
 
 function test_against_expected(fixture_name, expected_time, expected_deltime)
@@ -13,6 +13,6 @@ function test_against_expected(fixture_name, expected_time, expected_deltime)
     fixture_dir = fullfile(initial_dir, 'fixtures', fixture_name);
 
     [time,deltime]=modeltime(fixture_dir);
-    assert(time == expected_time, '%f (actual) != %f (expected)', time, expected_time);
-    assert(deltime == expected_deltime, '%f (actual) != %f (expected)', deltime, expected_deltime);
+    assert(round(time, 6) == expected_time, '%f (actual) != %f (expected)', time, expected_time);
+    assert(round(deltime, 6) == expected_deltime, '%f (actual) != %f (expected)', deltime, expected_deltime);
 end
