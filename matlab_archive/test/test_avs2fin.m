c8a11ef097b5acc87873c0a1f67191fc4a769d7e
Dustin <dmwinslow@users.noreply.github.com>
2022-01-25 09:59:33 -0800

    Copy supporting files for avs2fin test

--- matlab_archive/test/test_avs2fin.m
+++ matlab_archive/test/test_avs2fin.m
@@ -1,10 +1,10 @@
 % test avs2fin
 
-%% avs2fin np2d_cond
-test_against_fixture('np2d_cond', 'avs2fin_fixture.fin');
+%% avs2fin jdf2d_p12
+test_against_fixture('jdf2d_p12', 'p12', 'avs2fin_fixture.fin');
 
 
-function test_against_fixture(fixture_name, output_fixture_filename)
+function test_against_fixture(fixture_name, run_name, output_fixture_filename)
     initial_dir = pwd;
     fixture_dir = fullfile(initial_dir, 'fixtures', fixture_name);
     test_dir = fullfile(tempdir, strcat('test_avs2fin__', fixture_name));
@@ -16,8 +16,11 @@ function test_against_fixture(fixture_name, output_fixture_filename)
     try
         mkdir(test_dir);
         cd(test_dir);
+        
+        copyfile(fullfile(fixture_dir, strcat(run_name, '.ini')), '.');
+        copyfile(fullfile(fixture_dir, '*_sca_node.avs'), '.');
 
-        avs2fin('test.fin', fixture_dir);
+        avs2fin('test.fin');
 
         assert_files_match('test.fin', fullfile(fixture_dir, output_fixture_filename));
     catch e
