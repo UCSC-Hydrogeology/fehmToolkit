ee234fbdb315e36cdef545b001fedcc926b9e41e
Dustin <dmwinslow@gmail.com>
2022-02-14 12:17:25 +0000

    Build grid, node, and element objects

---
+++ fehm_toolkit/fehm_objects/__init__.py
@@ -0,0 +1,3 @@
+from .element import Element
+from .grid import Grid
+from .node import Node
---
+++ fehm_toolkit/fehm_objects/grid.py
@@ -0,0 +1,73 @@
+from .element import Element
+from .node import Node
+
+
+class Grid:
+    """Class representing a mesh or grid object."""
+
+    def __init__(self, nodes_by_number, elements_by_number):
+        self._nodes_by_number = nodes_by_number or {}
+        self._elements_by_number = elements_by_number or {}
+
+    def node(self, number):
+        try:
+            return self._nodes_by_number[number]
+        except KeyError:
+            raise KeyError(f'Node ({number}) not found in grid.')
+
+    def element(self, number):
+        try:
+            return self._elements_by_number[number]
+        except KeyError:
+            raise KeyError(f'Element ({number}) not found in grid.')
+
+    @property
+    def n_nodes(self):
+        return len(self._nodes_by_number)
+
+    @property
+    def n_elements(self):
+        return len(self._elements_by_number)
+
+    @classmethod
+    def from_fehm(cls, fehm_file):
+        nodes_by_number = None
+        elements_by_number = None
+
+        with open(fehm_file) as f:
+            while True:
+                block_name = next(f).strip()
+
+                if block_name == 'coor':
+                    nodes_by_number = cls._read_coor(f)
+                elif block_name == 'elem':
+                    elements_by_number = cls._read_elem(f)
+                elif block_name == 'stop':
+                    break
+                else:
+                    raise NotImplementedError(f'No parser for block type "{block_name}"')
+
+                next(f)  # throw away extra line after block
+
+        return cls(nodes_by_number=nodes_by_number, elements_by_number=elements_by_number)
+
+    @classmethod
+    def _read_coor(cls, open_file):
+        n_nodes = int(next(open_file))
+
+        nodes_by_number = {}
+        for i in range(n_nodes):
+            node = Node.from_fehm_line(next(open_file))
+            nodes_by_number[node.number] = node
+
+        return nodes_by_number
+
+    @classmethod
+    def _read_elem(cls, open_file):
+        n_elements = int(next(open_file).strip().split()[1])
+
+        elements_by_number = {}
+        for i in range(n_elements):
+            element = Element.from_fehm_line(next(open_file))
+            elements_by_number[element.number] = element
+        return elements_by_number
