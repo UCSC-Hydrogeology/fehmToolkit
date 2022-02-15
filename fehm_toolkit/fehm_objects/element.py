8d7ddd838ac385831d807d132a7ddec1ecaf3c34
Dustin <dmwinslow@gmail.com>
2022-02-15 16:52:00 +0000

    Fix bug in element parsing

--- fehm_toolkit/fehm_objects/element.py
+++ fehm_toolkit/fehm_objects/element.py
@@ -12,4 +12,4 @@ class Element:
     def from_fehm_line(cls, raw_line: str) -> 'Element':
         split_line = raw_line.strip().split()
         n, nodes = split_line[0], split_line[1:]
-        return cls(number=int(n), connectivity=len(nodes), nodes=(int(node) for node in nodes))
+        return cls(number=int(n), connectivity=len(nodes), nodes=tuple(int(node) for node in nodes))
