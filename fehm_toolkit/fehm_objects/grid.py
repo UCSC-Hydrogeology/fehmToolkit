b799658fab37621fe0957b55c21340811f9f07ff
Dustin <dmwinslow@gmail.com>
2022-02-15 16:54:24 +0000

    Implement zone and area file readers

--- fehm_toolkit/fehm_objects/grid.py
+++ fehm_toolkit/fehm_objects/grid.py
@@ -107,3 +107,83 @@ def _read_elem(open_file: TextIO) -> dict[int, Node]:
         element = Element.from_fehm_line(next(open_file))
         elements_by_number[element.number] = element
     return elements_by_number
+
+
+def read_zones(zone_file: Path) -> tuple[dict, dict]:
+    """Read zone-formatted files (_outside.zone, _material.zone, .area)
+
+    Read zone-based values (node numbers, areas), and return these as a dictionary keyed by zone number. Also construct
+    a mapping dictionary between the zone name and zone number.
+    """
+
+    zone_number_by_name = {}
+    node_values_by_zone_number = {}
+
+    with open(zone_file) as f:
+        file_header = next(f).strip()
+        if file_header != 'zone':
+            raise ValueError('Invalid zone file, must start with "zone" header')
+
+        while True:
+            zone_header = next(f).strip()
+            if _is_end_of_zone_file(zone_header, f):
+                break
+
+            zone_number, zone_name = _parse_zone_header(zone_header)
+            node_values = _read_zone(f)
+
+            node_values_by_zone_number[zone_number] = node_values
+            if zone_name:
+                zone_number_by_name[zone_name] = zone_number
+
+    return node_values_by_zone_number, zone_number_by_name
+
+
+def _read_zone(open_file: TextIO) -> tuple[int]:
+    nnum_header = next(open_file).strip()
+    if nnum_header != 'nnum':
+        raise ValueError(f'Invalid zone file, expected "nnum" instead of "{nnum_header}"')
+
+    n_nodes = int(next(open_file).strip())
+    node_values = []
+    while len(node_values) < n_nodes:
+        raw_values_for_line = next(open_file).strip().split()
+        if not node_values:
+            is_vector_format = _is_vector_formatted(raw_values_for_line, n_nodes)
+
+        values_for_line = _parse_zone_values_line(raw_values_for_line, is_vector_format=is_vector_format)
+        node_values.extend(values_for_line)
+
+    return tuple(node_values)
+
+
+def _parse_zone_header(header: str) -> tuple[int, Optional[str]]:
+    parsed = header.strip().split()
+    if len(parsed) == 1:
+        return int(parsed[0]), None
+
+    return int(parsed[0]), parsed[1]
+
+
+def _is_end_of_zone_file(header: str, open_file: TextIO) -> bool:
+    if not header:
+        if next(open_file).strip() != 'stop':
+            raise ValueError('Invalid zone file, unexpected blank line encountered')
+        return True
+    return False
+
+
+def _parse_zone_values_line(raw_line_values: list[str], is_vector_format: bool) -> tuple:
+    if is_vector_format:
+        first_vector = tuple(float(v) for v in raw_line_values[:3])
+        second_vector = tuple(float(v) for v in raw_line_values[4:7])
+        if not second_vector:
+            return [first_vector]
+        return [first_vector, second_vector]
+
+    return [int(v) for v in raw_line_values]
+
+
+def _is_vector_formatted(zone_line_values: list[str], n_nodes: int) -> bool:
+
+    return len(zone_line_values) == 6  # Lagrit writes two 3-vectors per line instead of the usual 10 scalars
