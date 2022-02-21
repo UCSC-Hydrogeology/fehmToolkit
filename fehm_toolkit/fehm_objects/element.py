0ae8144414a5b68687372eab07f1e89cb986ae60
Dustin <dmwinslow@gmail.com>
2022-02-21 10:03:26 +0000

    Add test for element, store nodes as set

--- fehm_toolkit/fehm_objects/element.py
+++ fehm_toolkit/fehm_objects/element.py
@@ -6,10 +6,14 @@ class Element:
     """Class representing grid elements (connectivity)."""
     number: int
     connectivity: int
-    nodes: tuple[int]
+    nodes: set[int]
 
     @classmethod
     def from_fehm_line(cls, raw_line: str) -> 'Element':
+        """ Construct Element from FEHM file line
+        >>> Element.from_fehm_line('4 1 2 3')
+        Element(number=4, connectivity=3, nodes={1, 2, 3})
+        """
         split_line = raw_line.strip().split()
         n, nodes = split_line[0], split_line[1:]
-        return cls(number=int(n), connectivity=len(nodes), nodes=tuple(int(node) for node in nodes))
+        return cls(number=int(n), connectivity=len(nodes), nodes=set(int(node) for node in nodes))
