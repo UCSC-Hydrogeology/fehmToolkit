f2c5e825a8da6fdac71ff2ba7f76dc149db4668a
Dustin <dmwinslow@gmail.com>
2022-05-10 11:16:31 +0100

    Simplify combine_state using built-ins

--- fehm_toolkit/utilities/create_restart.py
+++ fehm_toolkit/utilities/create_restart.py
@@ -29,23 +29,9 @@ def create_restart_from_avs(
     """
     base_restart_state, metadata = read_restart(base_restart_file)
     avs_state = read_avs(avs_file)
-    combined = _combine_state(avs_state, other=base_restart_state, property_kind_from_other='saturations')
+    combined = dataclasses.replace(avs_state, saturation=base_restart_state.saturation)
 
     if reset_model_time:
         metadata = dataclasses.replace(metadata, simulation_time_days=0)
 
     return combined, metadata
-
-
-def _combine_state(
-    state: State,
-    other: State,
-    property_kind_from_other: str,
-) -> State:
-    if property_kind_from_other not in {f.name for f in dataclasses.fields(State)}:
-        raise ValueError(f'property_kind {property_kind_from_other} is not supported by State')
-
-    new_values = dataclasses.asdict(other)[property_kind_from_other]
-    new_state = dataclasses.replace(state, **{property_kind_from_other: new_values})
-    new_state.validate()
-    return new_state
