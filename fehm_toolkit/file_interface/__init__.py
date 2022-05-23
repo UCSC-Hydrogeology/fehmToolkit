eed7407be6828f73db4056bbcb76b2192c97fc0f
Dustin <dmwinslow@gmail.com>
2022-05-23 16:26:26 +0100

    Implement reader for nist lookup tables

--- fehm_toolkit/file_interface/__init__.py
+++ fehm_toolkit/file_interface/__init__.py
@@ -1,6 +1,7 @@
 from .avs import read_avs
 from .compact_node_data import write_compact_node_data
 from .fehm import read_fehm
+from .fluid_properties import read_nist_lookup_table
 from .grid import read_grid
 from .pressure import read_pressure, write_pressure
 from .restart import read_restart, write_restart
