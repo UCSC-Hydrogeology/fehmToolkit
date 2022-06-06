aa9fb82aecb98b3b1255c8a91595e729b19ed932
Dustin <dmwinslow@gmail.com>
2022-06-06 08:31:55 +0100

    Implement scattered interpolant for pressure calculation

--- fehm_toolkit/hydrostatic_pressure.py
+++ fehm_toolkit/hydrostatic_pressure.py
@@ -15,12 +15,14 @@ logger = logging.getLogger(__name__)
 
 N_ITERATIONS = 3
 GRAVITY_ACCELERATION_M_S2 = -9.80665
+RANDOM_SAMPLE_SEED = 12
 
 
 def generate_hydrostatic_pressure_file(
     *,
     config_file: Path,
     fehm_file: Path,
+    material_zone_file: Path,
     outside_zone_file: Path,
     restart_file: Path,
     water_properties_file: Path,
@@ -32,32 +34,58 @@ def generate_hydrostatic_pressure_file(
     if pressure_config['model_kind'] not in ('depth'):
         raise NotImplementedError(f'model_kind {pressure_config["model_kind"]} not supported.')
 
-    logger.info('Parsing grid into memory')
-    grid = read_grid(fehm_file, outside_zone_file=outside_zone_file, read_elements=False)
+    logger.info('Reading node data into memory')
+    grid = read_grid(
+        fehm_file,
+        material_zone_file=material_zone_file,
+        outside_zone_file=outside_zone_file,
+        read_elements=False,
+    )
     state, restart_metadata = read_restart(restart_file)
-    density_lookup_MPa_degC = _read_density_lookup(water_properties_file)
 
-    node_numbers, node_coordinates, node_temperatures = _get_structured_node_data(grid, state)
+    coordinates_by_number = _get_coordinates_by_number_without_flat_dimensions(grid)
+    sampled_node_numbers = _sample_node_numbers(grid, sampling_configs=pressure_config.get('sampling_configs'))
+    sampled_node_coordinates, sampled_node_temperatures = _get_sampled_coordinate_and_temperature_arrays(
+        coordinates_by_number=coordinates_by_number,
+        state=state,
+        sampled_node_numbers=sampled_node_numbers,
+    )
+
+    logger.info('Reading water properties lookup')
+    density_lookup_MPa_degC = _read_density_lookup(water_properties_file)
 
     # TODO(dustin): Add config support for uniform temperature
     logger.info('Generating temperature lookups')
-    temperature_lookup = get_lookup_with_out_of_range_backup(points=node_coordinates, values=node_temperatures)
+    temperature_lookup = interpolate.NearestNDInterpolator(sampled_node_coordinates, sampled_node_temperatures)
 
-    logger.info('Bootstrapping node pressures')
+    logger.info(f'Calculating explicit pressures for {len(sampled_node_numbers)}/{len(coordinates_by_number)} nodes')
     pressure_by_node = {}
-    for i, (node_number, coordinates) in enumerate(zip(node_numbers, node_coordinates)):
-        if not i % 1000:
-            logger.info(f'Pressures calculated: {i} / {grid.n_nodes}')
+    for i, node_number in enumerate(sampled_node_numbers, start=1):
+        if not i % 10000 or i in (0, len(sampled_node_numbers)):
+            logger.info(f'Pressures calculated: {i} / {len(sampled_node_numbers)}')
         pressure_by_node[node_number] = _calculate_hydrostatic_pressure(
-            target_coordinates=coordinates,
+            target_coordinates=coordinates_by_number[node_number],
             params=pressure_config['model_params'],
             density_lookup_MPa_degC=density_lookup_MPa_degC,
             temperature_lookup=temperature_lookup,
             n_iterations=N_ITERATIONS,
         )
         if np.isnan(pressure_by_node[node_number]):
-            raise ValueError(f'Pressure for node {node_number} is not a number.')
+            raise ValueError(
+                f'Pressure for node {node_number} is not a number. May have gone out of range for density lookup.'
+            )
 
+    logger.info('Interpolating remaining node pressures')
+    pressure_lookup = get_lookup_with_out_of_range_backup(
+        points=np.array([coordinates_by_number[node_number] for node_number in pressure_by_node.keys()]),
+        values=np.fromiter(pressure_by_node.values(), dtype=float),
+    )
+    unassigned_nodes = coordinates_by_number.keys() - pressure_by_node.keys()
+    pressures = pressure_lookup(np.array([coordinates_by_number[node_number] for node_number in unassigned_nodes]))
+    for node, pressure in zip(unassigned_nodes, pressures):
+        pressure_by_node[node] = pressure
+
+    logger.info(f'Writing pressures to file {output_file}')
     write_pressure(pressure_by_node, output_file=output_file)
 
 
@@ -90,7 +118,7 @@ def _calculate_hydrostatic_pressure(
     mean_T = ((T_column + np.roll(T_column, -1)) / 2)[:-1]
     PT_column = _prepend_scalar_to_array(params['reference_pressure_MPa'], mean_T)
 
-    for iteration in range(n_iterations):
+    for iteration in range(n_iterations):  # TODO(Dustin): use convergence criteria rather than set number
         density_kg_m3 = density_lookup_MPa_degC(PT_column)
         delta_P = 1e-6 * density_kg_m3 * GRAVITY_ACCELERATION_M_S2 * signed_z_interval_m
         PT_column[:, 0] = params['reference_pressure_MPa'] + np.cumsum(delta_P)
@@ -111,28 +139,40 @@ def _prepend_scalar_to_array(scalar: float, z_column: np.array):
     return np.column_stack((tiled_xy, z_column))
 
 
-def _get_structured_node_data(grid: Grid, state: State) -> tuple[np.ndarray]:
+def _get_coordinates_by_number_without_flat_dimensions(grid: Grid) -> dict[int, np.ndarray]:
     numbers = []
     coordinates = []
-    temperatures = []
     for node in grid.nodes:
         numbers.append(node.number)
         coordinates.append(node.coordinates.value)
-        temperatures.append(state.temperature[node.number - 1])
 
-    numbers, coordinates, temperatures = np.array(numbers), np.array(coordinates), np.array(temperatures)
+    coordinates = np.array(coordinates)
     flat_dimension = _get_flat_dimension_or_none(coordinates)
     if flat_dimension is not None:
         coordinates = np.delete(coordinates, obj=flat_dimension, axis=1)
+    return {n: c for n, c in zip(numbers, coordinates)}
 
-    return numbers, coordinates, temperatures
+
+def _get_sampled_coordinate_and_temperature_arrays(
+    coordinates_by_number: dict[int, np.ndarray],
+    state: State,
+    sampled_node_numbers: set[int],
+) -> tuple[np.ndarray]:
+    sampled_coordinates = []
+    sampled_temperatures = []
+    for number, coordinate in coordinates_by_number.items():
+        if number in sampled_node_numbers:
+            sampled_coordinates.append(coordinate)
+            sampled_temperatures.append(state.temperature[number - 1])
+
+    return np.array(sampled_coordinates), np.array(sampled_temperatures)
 
 
 def _read_pressure_config(config_file: Path):
     with open(config_file) as f:
         config = yaml.load(f, Loader=yaml.Loader)
-        logger.info(f'Format for {config_file} is not YAML, trying legacy reader.')
     if not isinstance(config, dict):
+        logger.info(f'Format for {config_file} is not YAML, trying legacy reader.')
         config = read_legacy_ipi_config(config_file)
 
     return config['hydrostatic_pressure']
@@ -163,6 +203,40 @@ def get_lookup_with_out_of_range_backup(points: np.ndarray, values: np.ndarray) 
     return lookup_with_backup
 
 
+def _sample_node_numbers(grid: Grid, sampling_configs: Optional[list[dict]]):
+    sampled_nodes = set()
+    for config in sampling_configs:
+        sample_for_config = _get_sampled_nodes(grid, config)
+        sampled_nodes = sampled_nodes.union(sample_for_config)
+    return sampled_nodes
+
+
+def _get_sampled_nodes(grid: Grid, config: dict):
+    if config['zone_kind'] == 'outside':
+        zone_getter = grid.get_outside_zone
+    elif config['zone_kind'] == 'material':
+        zone_getter = grid.get_material_zone
+    else:
+        raise NotImplementedError(f"No support for zone_kind {config['zone_kind']}")
+
+    rng = np.random.default_rng(RANDOM_SAMPLE_SEED)
+    sampled_nodes = set()
+    for zone_key in config['zones']:
+        zone = zone_getter(zone_key)
+
+        if config['sample_method'] == 'number':
+            sample_size = config['sample_size']
+        elif config['sample_method'] == 'fraction':
+            sample_size = int(config['sample_size'] * len(zone.data)) or 1
+        else:
+            raise NotImplementedError(f"No support for sample_method {config['sample_method']}")
+
+        zone_sample = rng.choice(zone.data, size=sample_size, replace=False)
+        sampled_nodes = sampled_nodes.union(set(zone_sample))
+
+    return sampled_nodes
+
+
 def _get_flat_dimension_or_none(coordinates: np.array) -> Optional[int]:
     """Identify which (if any) of the first two dimensions has no variance.
     >>> import numpy as np
@@ -184,6 +258,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--config_file', type=Path, help='Path to configuration (.yaml/.ipi) file.')
     parser.add_argument('--fehm_file', type=Path, help='Path to main grid (.fehm) file.')
+    parser.add_argument('--material_zone_file', type=Path, help='Path to material (_material.zone) file.')
     parser.add_argument('--outside_zone_file', type=Path, help='Path to boundary (_outside.zone) file.')
     parser.add_argument('--restart_file', type=Path, help='Path to restart (.fin/.ini) file.')
     parser.add_argument('--water_properties_file', type=Path, help='Path to NIST lookup table (.out/.wpi).')
@@ -193,6 +268,7 @@ if __name__ == '__main__':
     generate_hydrostatic_pressure_file(
         config_file=args.config_file,
         fehm_file=args.fehm_file,
+        material_zone_file=args.material_zone_file,
         outside_zone_file=args.outside_zone_file,
         restart_file=args.restart_file,
         water_properties_file=args.water_properties_file,
