1bd9bf842c2493f0f8103e31c68acca9bbcfb8fd
Dustin <dmwinslow@gmail.com>
2022-08-17 09:31:11 +0100

    Update naming in create_config tests

--- test/end_to_end/test_create_config_for_legacy_run.py
+++ test/end_to_end/test_create_config_for_legacy_run.py
@@ -1,13 +1,13 @@
 from fehm_toolkit.config import RunConfig
-from fehm_toolkit.utilities.create_run_config_for_legacy_directory import create_run_config_for_legacy_directory
+from fehm_toolkit.fehm_runs.create_config_for_legacy_run import create_config_for_legacy_run
 
 
-def test_create_run_config_for_legacy_directory_flat_box(tmp_path, end_to_end_fixture_dir):
+def test_create_config_for_legacy_run_flat_box(tmp_path, end_to_end_fixture_dir):
     run_directory = end_to_end_fixture_dir / 'flat_box' / 'p12'
 
     fixture_file = run_directory / 'config.yaml'
     output_file = tmp_path / 'config.yaml'
-    create_run_config_for_legacy_directory(
+    create_config_for_legacy_run(
         directory=run_directory,
         config_file=output_file,
         input_file=run_directory / 'p12.dat',  # specified to avoid other test fixtures in folder
@@ -17,12 +17,12 @@ def test_create_run_config_for_legacy_directory_flat_box(tmp_path, end_to_end_fi
     assert RunConfig.from_yaml(output_file) == RunConfig.from_yaml(fixture_file)
 
 
-def test_create_run_config_for_legacy_directory_outcrop_2d(tmp_path, end_to_end_fixture_dir):
+def test_create_config_for_legacy_run_outcrop_2d(tmp_path, end_to_end_fixture_dir):
     run_directory = end_to_end_fixture_dir / 'outcrop_2d' / 'p13'
 
     fixture_file = run_directory / 'config.yaml'
     output_file = tmp_path / 'config.yaml'
-    create_run_config_for_legacy_directory(
+    create_config_for_legacy_run(
         directory=run_directory,
         config_file=output_file,
         input_file=run_directory / 'p13.dat',  # specified to avoid other test fixtures in folder
