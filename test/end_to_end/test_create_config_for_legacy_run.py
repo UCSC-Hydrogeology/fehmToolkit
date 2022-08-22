69ea4ca79678acd68deb52cd34c0e433f4cf4614
Dustin <dmwinslow@gmail.com>
2022-08-22 11:27:19 +0100

    Improve Windows compatibility for relative paths

--- test/end_to_end/test_create_config_for_legacy_run.py
+++ test/end_to_end/test_create_config_for_legacy_run.py
@@ -8,36 +8,38 @@ def test_create_config_for_legacy_run_flat_box(tmp_path, end_to_end_fixture_dir)
     run_directory = end_to_end_fixture_dir / 'flat_box' / 'p12'
     tmp_model_dir = tmp_path / 'root' / 'model' / 'run'
     shutil.copytree(run_directory, tmp_model_dir)
-    (tmp_model_dir / '../../nist120-1800.out').touch()
+    (tmp_model_dir.parent.parent / 'nist120-1800.out').touch()
 
-    fixture_file = run_directory / 'config.yaml'
     output_file = tmp_model_dir / 'config.yaml'
+    fixture_file = tmp_model_dir / 'fixture.yaml'
+    output_file.rename(fixture_file)
+
     create_config_for_legacy_run(
         directory=tmp_model_dir,
         config_file=output_file,
         input_file=tmp_model_dir / 'p12.dat',  # specified to avoid other test fixtures in folder
         final_conditions_file=tmp_model_dir / 'p12.fin',  # specified to avoid other test fixtures in folder
-        water_properties_file=tmp_model_dir / '../../nist120-1800.out',
+        water_properties_file=tmp_model_dir.parent.parent / 'nist120-1800.out',
     )
-    RunConfig.from_yaml(output_file)  # loading ensures correct structure
-    assert output_file.read_text() == fixture_file.read_text()
+    assert RunConfig.from_yaml(output_file) == RunConfig.from_yaml(fixture_file)
 
 
 def test_create_config_for_legacy_run_outcrop_2d(tmp_path, end_to_end_fixture_dir):
     run_directory = end_to_end_fixture_dir / 'outcrop_2d' / 'p13'
     tmp_model_dir = tmp_path / 'root' / 'model' / 'run'
     shutil.copytree(run_directory, tmp_model_dir)
-    (tmp_model_dir / '../../nist120-1800.out').touch()
+    (tmp_model_dir.parent.parent / 'nist120-1800.out').touch()
 
-    fixture_file = run_directory / 'config.yaml'
     output_file = tmp_model_dir / 'config.yaml'
+    fixture_file = tmp_model_dir / 'config.yaml'
+    output_file.rename(fixture_file)
+
     create_config_for_legacy_run(
         directory=tmp_model_dir,
         config_file=output_file,
         input_file=tmp_model_dir / 'p13.dat',  # specified to avoid other test fixtures in folder
         initial_conditions_file=tmp_model_dir / 'p13.ini',  # specified to avoid other test fixtures in folder
         final_conditions_file=tmp_model_dir / 'p13.fin',  # specified to avoid other test fixtures in folder
-        water_properties_file=tmp_model_dir / '../../nist120-1800.out',
+        water_properties_file=tmp_model_dir.parent.parent / 'nist120-1800.out',
     )
-    RunConfig.from_yaml(output_file)  # loading ensures correct structure
-    assert output_file.read_text() == fixture_file.read_text()
+    assert RunConfig.from_yaml(output_file) == RunConfig.from_yaml(fixture_file)
