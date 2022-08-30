5639e27ef0a62a4a8038dbe93eaa83ec550a9826
Dustin <dmwinslow@gmail.com>
2022-08-30 12:17:04 +0100

    Implement create run from run

--- test/end_to_end/test_create_runs.py
+++ test/end_to_end/test_create_runs.py
@@ -1,6 +1,7 @@
-from fehm_toolkit.config import FilesConfig
-from fehm_toolkit.fehm_runs import create_run_from_mesh
+from fehm_toolkit.config import FilesConfig, RunConfig
+from fehm_toolkit.fehm_runs import create_run_from_mesh, create_run_from_run
 from fehm_toolkit.fehm_runs.create_run_from_mesh import create_template_input_file
+from fehm_toolkit.file_interface import read_restart
 
 
 def test_create_run_from_mesh_flat_box_infer(tmp_path, end_to_end_fixture_dir):
@@ -113,3 +114,36 @@ def test_create_template_input_file(tmp_path):
         f'ppor\nfile\n{files_config.pore_pressure}\n'
         'stop\n'
     )
+
+
+def test_create_run_from_run(tmp_path, end_to_end_fixture_dir):
+    run_directory = end_to_end_fixture_dir / 'outcrop_2d' / 'cond'
+    run_config_file = run_directory / 'config.yaml'
+    new_directory = tmp_path / 'new_run'
+    new_config_file = new_directory / 'config.yaml'
+
+    create_run_from_run(run_config_file, new_directory)
+
+    run_config = RunConfig.from_yaml(run_config_file)
+    new_config = RunConfig.from_yaml(new_config_file)
+
+    assert new_config.files_config.material_zone.exists()
+    assert new_config.files_config.outside_zone.exists()
+    assert new_config.files_config.area.exists()
+    assert new_config.files_config.rock_properties.exists()
+    assert new_config.files_config.conductivity.exists()
+    assert new_config.files_config.pore_pressure.exists()
+    assert new_config.files_config.permeability.exists()
+    assert new_config.files_config.water_properties.exists() and new_config.files_config.water_properties.is_symlink()
+    assert new_config.files_config.grid.exists()
+    assert new_config.files_config.store.exists()
+    assert new_config.files_config.files.exists()
+    assert new_config.files_config.input.exists()
+
+    assert new_config.files_config.grid.read_text() == run_config.files_config.grid.read_text()
+    assert new_config.files_config.outside_zone.read_text() == run_config.files_config.outside_zone.read_text()
+    assert new_config.files_config.area.read_text() == run_config.files_config.area.read_text()
+    assert new_config.files_config.rock_properties.read_text() == run_config.files_config.rock_properties.read_text()
+    new_initial_state, _ = read_restart(new_config.files_config.initial_conditions)
+    run_final_state, _ = read_restart(run_config.files_config.final_conditions)
+    assert new_initial_state == run_final_state
