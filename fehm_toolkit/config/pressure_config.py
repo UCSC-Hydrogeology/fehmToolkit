525eead15fbb7d76be42335a1fa0318427f31ddc
Dustin <dmwinslow@gmail.com>
2022-09-13 16:26:07 +0100

    Make interpolation model optional on pressure config

--- fehm_toolkit/config/pressure_config.py
+++ fehm_toolkit/config/pressure_config.py
@@ -7,13 +7,15 @@ from .model_config import ModelConfig
 @dataclass
 class PressureConfig:
     pressure_model: ModelConfig
-    interpolation_model: ModelConfig
+    interpolation_model: ModelConfig = None
     sampling_model: Optional[ModelConfig] = None
 
     @classmethod
     def from_dict(cls, dct):
         return cls(
             pressure_model=ModelConfig.from_dict(dct['pressure_model']),
-            interpolation_model=ModelConfig.from_dict(dct['interpolation_model']),
+            interpolation_model=(
+                ModelConfig.from_dict(dct['interpolation_model']) if dct.get('interpolation_model') else None
+            ),
             sampling_model=ModelConfig.from_dict(dct['sampling_model']) if dct.get('sampling_model') else None,
         )
