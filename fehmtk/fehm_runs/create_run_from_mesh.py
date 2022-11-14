543a88041a87ef0f4517e065840169d05be01e56
Dustin <dmwinslow@gmail.com>
2022-11-14 17:18:35 +0000

    Create run_from_mesh files config relative to target

--- fehmtk/fehm_runs/create_run_from_mesh.py
+++ fehmtk/fehm_runs/create_run_from_mesh.py
@@ -79,7 +79,7 @@ def create_run_from_mesh(
     template_files_config = get_template_files_config(file_pairs_by_file_type, run_root)
     create_template_run_config(template_files_config, output_file=target_directory / CONFIG_NAME)
 
-    files_config = _files_config_from_template(template_files_config)
+    files_config = _files_config_from_template(template_files_config, target_directory)
 
     logger.info('Writing files index to %s', target_directory / files_config.files.name)
     write_files_index(files_config, output_file=target_directory / files_config.files.name)
@@ -229,10 +229,10 @@ def get_template_files_config(
     return template
 
 
-def _files_config_from_template(template_files_config: dict) -> FilesConfig:
+def _files_config_from_template(template_files_config: dict, target_directory: Path) -> FilesConfig:
     config_dict = {}
     for key, value in template_files_config.items():
         (required_str, file_name) = key.split('__')
         if not value.startswith('TYPE__'):
             config_dict[file_name] = value
-    return FilesConfig.from_dict(config_dict)
+    return FilesConfig.from_dict(config_dict, files_relative_to=target_directory)
